d35997377ae8d934995bb983033d6117d108699f
Peter Alfonso <peteralfonsojr@gmail.com>
2012-04-09 11:19:49 -0400

    Change zip and print output, remove TTS files

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,7 +39,7 @@ PRODUCT_LOCALES := en_US
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage7.mk)
 
 # Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+# $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
